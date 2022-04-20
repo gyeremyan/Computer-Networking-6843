da65303f6cb3c1b9001454605cc108b89d583bc3
George Yeremian <61395564+gyeremyan@users.noreply.github.com>
2022-04-19 21:24:10 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ import binascii
 ICMP_ECHO_REQUEST = 8
 MAX_HOPS = 30
 TIMEOUT = 2.0
-TRIES = 1
+TRIES = 2
 # The packet that we shall send to each router along the path is the ICMP echo
 # request packet, which is exactly what we had used in the ICMP ping exercise.
 # We shall use the same packet that we built in the Ping exercise
